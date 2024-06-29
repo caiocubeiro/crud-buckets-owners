1987426cc351d1ae9b22bd6571a60bd3f9d52ea5
Caio Cubeiro <caiocubeiroedu@gmail.com>
2024-06-29 15:46:06 -0300

    creating insigths dashboard

--- app/flask/db/bucket_owners.sql
+++ app/flask/db/bucket_owners.sql
@@ -3,9 +3,8 @@ CREATE DATABASE buckets_owners;
 CREATE TABLE buckets (
     uuid CHAR(36) PRIMARY KEY,
     bucket_name VARCHAR(255) NOT NULL,
+    size_gb DECIMAL(10,2) NOT NULL,
     owner_uuid CHAR(36) NOT NULL,
-    owner_name VARCHAR(255) NOT NULL,
-    owner_email VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
 );
@@ -14,6 +13,7 @@ CREATE TABLE owners (
     uuid CHAR(36) NOT NULL PRIMARY KEY,
     owner_name VARCHAR(255) NOT NULL,
     owner_email VARCHAR(255) NOT NULL UNIQUE,
+    owner_role VARCHAR(255) NOT NULL,
     owner_department VARCHAR(255),
     owner_manager VARCHAR(255),
     created_at TIMESTAMP NOT NULL,
